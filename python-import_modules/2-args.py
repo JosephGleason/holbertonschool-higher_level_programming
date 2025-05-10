7678bc67db4dcd20f7d4000ca111ce86a19b61dd
JosephGleason <9719@holbertonstudents.com>
2025-05-10 17:56:44 -0400

    added for loop for stings

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import sys
 
-arg_count = len(sys.argv) - 1
-if arg_count == 0:
-    print('0 arguments.')
-elif arg_count == 1:
-    print('1 argument:')
-else:
-    print(f'{arg_count} arguments:')
+if __name__ == "__main__":
+    arg_count = len(sys.argv) - 1
+    if arg_count == 0:
+        print('0 arguments.')
+    elif arg_count == 1:
+        print('1 argument:')
+    else:
+        print(f'{arg_count} arguments:')
+    for i in range(1, arg_count + 1):
+        print(f'{i}: {sys.argv[i]}')
