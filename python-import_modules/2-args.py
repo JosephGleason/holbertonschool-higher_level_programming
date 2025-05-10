0e4d8514756d91f2c854e9d31fd70cad8eeedb69
JosephGleason <9719@holbertonstudents.com>
2025-05-10 17:50:07 -0400

    added program that prints the number of and the list of its arguments.

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+arg_count = len(sys.argv) - 1
+if arg_count == 0:
+    print('0 arguments.')
+elif arg_count == 1:
+    print('1 argument:')
+else:
+    print(f'{arg_count} arguments:')
