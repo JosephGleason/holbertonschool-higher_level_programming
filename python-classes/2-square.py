09ab97f16646e71d518e00c8b076d0a3c7d649f4
josephg <9719@holbertonstudents.com>
2025-05-27 11:30:45 -0700

    fixed whitespaces

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
         """Initialize a new square with a given size.
         Args:
            size (int): The size of the square (default is 0).
-           
+
         Raises:
            TypeError: If size is not an integer.
            ValueError: If size is less than 0.
