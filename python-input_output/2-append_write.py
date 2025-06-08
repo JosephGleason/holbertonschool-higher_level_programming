211cff433ff3f7efe3a929b793d8a77d1bac2c9f
JosephGleason <9719@holbertonstudents.com>
2025-06-08 15:26:27 -0400

    fix for 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Defines a function that overwrites a file"""
+"""Defines a function that appends a string to a file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """
-    Writes a string to a text file (UTF8)
-    and returns the number of characters written:
+    Appends a string to a text file (UTF8)
+    and returns the number of characters added:
     Args:
-        filename (str): name of the file to write
-        text (str): string to write to the file
+        filename (str): name of the file to append to
+        text (str): string to append to the file
     Returns:
-        int: the number of characters written
+        int: the number of characters added
     """
 
-    with open(filename, 'w', encoding="utf-8") as file:
+    with open(filename, 'a', encoding="utf-8") as file:
         return file.write(text)
