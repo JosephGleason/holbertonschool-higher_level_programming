821c083b9b4b874e9a9975fa712960fcb50bb80e
JosephGleason <9719@holbertonstudents.com>
2025-06-29 19:20:21 -0400

    fix descrip

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,6 @@
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- 1. Root user
+-- Create user_0d_1@localhost with password user_0d_1_pwd if it doesn’t exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant ALL privileges on all databases and tables with grant option
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
