1a79267b7e53b698b1fc8e621b04f3d0cc04ac12
JosephGleason <9719@holbertonstudents.com>
2025-06-29 19:24:28 -0400

    added 0 task

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,3 @@
--- 1. Root user
--- Create user_0d_1@localhost with password user_0d_1_pwd if it doesn’t exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant ALL privileges on all databases and tables with grant option
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- Lists privileges of the MySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
