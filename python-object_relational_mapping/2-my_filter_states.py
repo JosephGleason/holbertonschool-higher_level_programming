fd449b1121cdd68c775e0954e8e0cfd2d68a62ac
JosephGleason <9719@holbertonstudents.com>
2025-07-02 17:58:28 -0400

    take 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,47 @@
 #!/usr/bin/python3
-"""Takes argument and displays all values in the states"""
-import MySQLdb
+"""
+Display all values in the states table where name matches the given argument.
+Usage: ./2-my_filter_states.py <mysql username>
+<mysql password> <database name> <state name searched>
+"""
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    # Get command-line arguments
+    mysql_user = sys.argv[1]
+    mysql_passwd = sys.argv[2]
+    mysql_db = sys.argv[3]
     state_name = sys.argv[4]
 
-    # SQL connection
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            user=mysql_user,
+            passwd=mysql_passwd,
+            db=mysql_db,
+            port=3306
+        )
 
-    cur = db.cursor()
-    query = ("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC").format(state_name)
-    cur.execute(query)
+        # Create a cursor object
+        cursor = db.cursor()
 
-    for row in cur.fetchall():
-        print(row)
+        # Execute SQL query to filter states
+        # matching the argument
+        query = """SELECT * FROM states
+            WHERE  BINARY name = '{}' ORDER BY id ASC;""".format(
+            state_name
+        )
+        cursor.execute(query)
 
-    # Exits cursor
-    cur.close()
-    db.close()
+        # Fetch all results and print them
+        for row in cursor.fetchall():
+            print(row)
+
+        # Close cursor and database connection
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL: {e}")
