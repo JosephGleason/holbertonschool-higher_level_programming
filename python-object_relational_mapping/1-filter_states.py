96c1691df48e9940a6e3df961476a27c28be34ca
JosephGleason <9719@holbertonstudents.com>
2025-07-02 16:46:55 -0400

    fixed header

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
-"""
+""" Lists states that start with 'N' """
 import MySQLdb
 import sys
 
