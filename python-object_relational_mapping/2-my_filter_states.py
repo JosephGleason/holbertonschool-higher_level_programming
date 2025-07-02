7073ea3aed8674856795552ef5e9f462477b72fd
JosephGleason <9719@holbertonstudents.com>
2025-07-02 18:00:21 -0400

    added binary

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,47 +1,39 @@
 #!/usr/bin/python3
 """
-Display all values in the states table where name matches the given argument.
-Usage: ./2-my_filter_states.py <mysql username>
-<mysql password> <database name> <state name searched>
+Lists all states from the database hbtn_0e_0_usa
+whose name matches the provided argument.
 """
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    # Get command-line arguments
-    mysql_user = sys.argv[1]
-    mysql_passwd = sys.argv[2]
-    mysql_db = sys.argv[3]
+    # Assigning arguments to the requested variable names
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    try:
-        # Connect to MySQL server
-        db = MySQLdb.connect(
-            host="localhost",
-            user=mysql_user,
-            passwd=mysql_passwd,
-            db=mysql_db,
-            port=3306
-        )
+    # Connects to SQL on localhost
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
 
-        # Create a cursor object
-        cursor = db.cursor()
+    # Creates a cursor to get SQL queries
+    cur = db.cursor()
 
-        # Execute SQL query to filter states
-        # matching the argument
-        query = """SELECT * FROM states
-            WHERE  BINARY name = '{}' ORDER BY id ASC;""".format(
-            state_name
-        )
-        cursor.execute(query)
+    query = (
+        "SELECT * FROM states WHERE BINARY sname = '{}' ORDER BY id ASC"
+        ).format(state_name)
+    # Executes SQL query that gets states
+    cur.execute(query)
 
-        # Fetch all results and print them
-        for row in cursor.fetchall():
-            print(row)
+    # Prints list of states
+    for row in cur.fetchall():
+        print(row)
 
-        # Close cursor and database connection
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL: {e}")
+    cur.close()
+    db.close()
