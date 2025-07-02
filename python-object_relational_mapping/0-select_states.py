00df861d8fca73ec8e9aa81265f3971558b13ee8
JosephGleason <9719@holbertonstudents.com>
2025-07-02 14:43:25 -0400

    fixed extra connection charset

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -17,8 +17,7 @@ if __name__ == "__main__":
         port=3306,
         user=mysql_username,
         passwd=mysql_password,
-        db=database_name,
-        charset="utf8"
+        db=database_name
     )
     
     # Create a cursor object/ used to run sql cmnds
