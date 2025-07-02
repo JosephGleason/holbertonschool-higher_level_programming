fd9b07603eb865ba4c85246cb5a14d95f76d43c5
JosephGleason <9719@holbertonstudents.com>
2025-07-02 16:39:37 -0400

    added 1. Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Assigning arguments to the requested variable names
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connects to SQL on localhost
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    # Creates a cursor to get SQL queries
+    cur = db.cursor()
+
+    # Executes SQL query that gets states
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Prints list of states
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
+    
