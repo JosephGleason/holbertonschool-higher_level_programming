1407b1523b446b1243545215820a40d95a25a7ff
JosephGleason <9719@holbertonstudents.com>
2025-07-02 14:50:23 -0400

    added code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,49 +7,28 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Ensure correct number of command-line arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py <mysql_username> <mysql_password> <mysql_database>", file=sys.stderr)
-        sys.exit(1)
-
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    db = None # Initialize db to None for finally block
-    cur = None # Initialize cur to None for finally block
+    # Connects to SQL on localhost
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
 
-    try:
-        # Connects to SQL on localhost
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=db_name
-        )
+    # Creates a cursor to get SQL queries
+    cur = db.cursor()
 
-        # Creates a cursor to get SQL queries
-        cur = db.cursor()
+    # Excutes SQL querie that gets states
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-        # Executes SQL querie that gets states
-        cur.execute("SELECT id, name FROM states ORDER BY id ASC") 
+    # Prints list of states
+    for row in cur.fetchall():
+        print(row)
 
-        # Prints list of states
-        for row in cur.fetchall():
-            print(row)
-
-    except MySQLdb.OperationalError as e:
-        # This will catch connection errors and print them
-        print(f"MySQL Error: {e}", file=sys.stderr) # Print to stderr for errors
-        sys.exit(1)
-    except Exception as e:
-        # Catch any other unexpected errors
-        print(f"An unexpected error occurred: {e}", file=sys.stderr)
-        sys.exit(1)
-    finally:
-        # Ensure cursor and database connections are closed
-        if cur:
-            cur.close()
-        if db:
-            db.close()
+    cur.close()
+    db.close()
