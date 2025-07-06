42ac659b5a77b24bd53b6fec04c238b204600507
JosephGleason <9719@holbertonstudents.com>
2025-07-06 19:48:18 -0400

    fixed pycodestyle

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -12,18 +12,18 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    
+
     engine = create_engine(
         f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
         pool_pre_ping=True
     )
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     new_state = State(name="Louisiana")
-    session.add(new_state) # stages the object for insertion
-    session.commit() # INSERT SQL query is actually executed
-    print(new_state.id) # new_state object will have its id populated
-    
+    session.add(new_state)  # stages the object for insertion
+    session.commit()  # INSERT SQL query is actually executed
+    print(new_state.id)  # new_state object will have its id populated
+
     session.close()
