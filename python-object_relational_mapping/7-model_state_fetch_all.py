954cd6ee304d42ae811cf9be9d68caefa5fdc3a2
JosephGleason <9719@holbertonstudents.com>
2025-07-06 17:15:37 -0400

    indentation fix

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,15 +13,15 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
-        pool_pre_ping=True
-    )
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
+            pool_pre_ping=True
+        )
 
-Session = sessionmaker(bind=engine)
-session = Session()
-states = session.query(State).order_by(State.id).all()
-for state in states:
-    print(f"{state.id}: {state.name}")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
 
-session.close()
+    session.close()
