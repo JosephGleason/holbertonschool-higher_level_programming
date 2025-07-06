ad908004b1cc597eff08affbb36087d63b33c6ef
JosephGleason <9719@holbertonstudents.com>
2025-07-06 19:44:10 -0400

    fix

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,32 +1,54 @@
 #!/usr/bin/python3
 """
-Prints the State object with the id passed as an argument
+Prints the State object's id with the given name
+from the database hbtn_0e_6_usa.
+(SQL injection free)
+
+Usage:
+    ./10-model_state_my_get.py
+    <mysql_username> <mysql_password> <database_name> <state_name>
+
+- Connects to a MySQL server running on localhost at port 3306.
+- If the state name is found, prints its id.
+- Otherwise, prints "Not found".
 """
+
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # Get MySQL credentials and state name from command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    state = sys.argv[4]
+    searched_state = sys.argv[4]
 
-    # Create the engine
+    # Create an SQLAlchemy engine
     engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}",
         pool_pre_ping=True
     )
 
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    get_state = session.query(State).filter(State.id == state).first()
+    # Query for the first State object with the name = searched_state
+    # Using .filter() with Python variables is safe from SQL injection
+    # in SQLAlchemy
+    found_state = (
+        session.query(State)
+        .filter(State.name == searched_state)
+        .first()
+    )
 
-    if get_state:
-        print(f"{get_state.id}")
+    # Print the result
+    if found_state:
+        print(found_state.id)
     else:
         print("Not found")
 
+    # Close the session
     session.close()
