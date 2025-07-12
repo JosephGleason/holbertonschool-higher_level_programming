77dfe980508a5652e02212c847da2cf4b0d8dea8
JosephGleason <9719@holbertonstudents.com>
2025-07-12 14:50:24 -0400

    added another fix

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,20 +5,17 @@ const args = process.argv.slice(2);
 if (args.length <= 1) {
   console.log(0);
 } else {
-  // Convert all args to integers
   const numbers = args.map(function (arg) {
     return parseInt(arg);
   });
 
-  // Sort numbers in descending order
   const sortedNumbers = numbers.sort(function (a, b) {
     return b - a;
   });
 
-  // Create a new array for unique numbers
-  var uniqueNumbers = [];
+  let uniqueNumbers = [];
 
-  for (var i = 0; i < sortedNumbers.length; i++) {
+  for (let i = 0; i < sortedNumbers.length; i++) {
     if (uniqueNumbers.indexOf(sortedNumbers[i]) === -1) {
       uniqueNumbers.push(sortedNumbers[i]);
     }
