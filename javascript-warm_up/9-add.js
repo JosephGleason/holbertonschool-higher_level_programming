3184b996aee9065d655b32be49b239e523146903
JosephGleason <9719@holbertonstudents.com>
2025-07-12 13:25:32 -0400

    wrong space dd

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,8 +3,8 @@
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
-console.log(add (num1, num2));
+console.log(add(num1, num2));
